5811ab543154fe8ed3cbf7335073542bcb69a71e
Md Niaz Morshed <md.niaz.morshed@uni-potsdam.de>
2024-07-09 01:15:55 +0200

    updated page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { Text, Box, Container } from '@radix-ui/themes';
 const Home: FC = () => {
   return (
     <div>
-      <Box variant="surface" style={{ backdropFilter: 'blur(10px)', boxShadow: 'var(--shadow-1)', background: 'var(--mauve-a4)' }}>
+      <Box style={{ backdropFilter: 'blur(10px)', boxShadow: 'var(--shadow-1)', background: 'var(--mauve-a4)' }}>
         <Container size="1" align='center'>
           <Text as="p" size="8" my='8' align='center'>AI Survey Analysis</Text>
         </Container>
